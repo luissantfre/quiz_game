8892ed1da728484bbf749a403d1cb8f710c4e4ae
luissantfre <fs2480570@gmail.com>
2025-06-12 09:41:37 -0300

    update

--- logica.js
+++ logica.js
@@ -12,8 +12,8 @@ perguntas = [
     ],
     correta: 1
    },
-   {
-   pergunta: "Qual a primeira capital do Brasil",
+  {
+   pergunta: "Qual a primeira capital do Brasil?",
    opcoes: [
      "Taquara",
      "Varjota",
@@ -22,23 +22,23 @@ perguntas = [
    ],
    correta: 2
    },
-   {
-   pergunta: "Qual é a força de mordida do Tiranossauro Rex",
+  {
+   pergunta: "Qual é a força de mordida do Tiranossauro Rex?",
    opcoes: [
      "35.000 newtons",
      "50.000 newtons",
-     "500 newtons",
-     "-0,25 newtons"
+     "10.500 newtons",
+     "75.000newtons"
    ],
    correta: 0
    },
-   {
+  {
      pergunta: "Quais foram os principais fatores para a extinção dos dinossauros?",
      opcoes: [
        "Os peidos deles, efeito estufa e falta de alimento",
-       "oi",
-       "Impacto do asteroide, erupções vulcanicas e mudanças climáticas",
-       "ola"
+       "falta de alimento, atmosfera poluida pela atividade humana e tsunamis",
+       "Impacto do asteroide, erupções vulcânicas e mudanças climáticas",
+       "aumento do nivel do mar, erupções vulcânicas e falta de alimento "
        
      ],
      correta: 2
@@ -57,7 +57,7 @@ botaoIniciar = document.querySelector(".botao")
 botaoIniciar.addEventListener("click", iniciarJogo)
 
 
-divPergunta = document.querySelector(".pergunta")
+divPergunta = document.querySelector(".fala")
 
 botao0 = document.querySelector(".opcao0")
 botao1 = document.querySelector(".opcao1")
@@ -66,7 +66,12 @@ botao3 = document.querySelector(".opcao3")
 
 telaAcertou.addEventListener("click", () => {
   telaAcertou.style.display = "none"
-  iniciarJogo()
+  telaPergunta.style.display = "block"
+})
+
+telaErrou.addEventListener("click", () => {
+  telaErrou.style.display = "none"
+  telaPergunta.style.display = "block"
 })
 
 botao0.addEventListener("click", () => {
@@ -114,7 +119,6 @@ botaoIniciar.addEventListener("click", iniciarJogo)
 
 function iniciarJogo() {
  
-  telaInicial = document.querySelector(".tela-inicial")
   telaInicial.style.display = "none"
   telaPergunta.style.display = "block"
 
@@ -125,23 +129,23 @@ function mostrarPergunta() {
   objPergunta = perguntas[0]
   
   textoPergunta = document.createTextNode(objPergunta.pergunta)
-  divPergunta = document.querySelector(".fala")
+  
   divPergunta.appendChild(textoPergunta)
   
   txtOpcao1 = document.createTextNode(objPergunta.opcoes[0])
-  botao0 = document.querySelector(".opcao0")
+  
   botao0.appendChild(txtOpcao1)
   
   txtOpcao2 = document.createTextNode(objPergunta.opcoes[1])
-  botao1 = document.querySelector(".opcao1")
+  
   botao1.appendChild(txtOpcao2)
   
   txtOpcao3 = document.createTextNode(objPergunta.opcoes[2])
-  botao2 = document.querySelector(".opcao2")
+
   botao2.appendChild(txtOpcao3 )
   
   txtOpcao4 = document.createTextNode(objPergunta.opcoes[3])
-  botao3 = document.querySelector(".opcao3")
+  
   botao3.appendChild(txtOpcao4)
   
   console.log(pergunta.pergunta)
